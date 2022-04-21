0f7932a9723c1492fe9b7b1483072a0bf7ac735c
karin <you@example.com>
2022-04-20 21:02:49 -0400

    functionality

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,29 +1,17 @@
 package controller;
 
 
-import java.io.FileWriter;
-
 import com.group7.ist.custmngmnt.App;
 
 import java.io.IOException;
-import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-
 import javafx.fxml.FXML;
-
 import javafx.scene.control.TextField;
-
-import javafx.scene.control.TextField;
-
-
 
 public class LoginController {
 
-
     //import the scene builders controls
-    
     @FXML
     private TextField usernameTextField;
 
@@ -32,50 +20,48 @@ public class LoginController {
 
     @FXML
     private Button loginButton;
-    
+
     @FXML
     private Button registerButton;
-    
+
     @FXML
     private Label usernameLabel;
-    
+
     @FXML
     private Label passwordLabel;
 
     @FXML
     private Label messageLabel;
-   
-
 
     @FXML
-                //method called when the login button is hit 
-    private void switchToSecondary() throws IOException {
-         
+    //method called when the login button is hit 
+    private void loginCustomer() throws IOException {
+
         //needs to do some type of check like this, might need more if else statments for caps and stuff
         //then searches the database or file for the customer to log them in
- if (usernameTextField.getText().isBlank() || passwordTextField.getText().isBlank()) {
-        messageLabel.setText("ERROR! Invalid!");
-        } else {
-          String username = usernameTextField.getText();
-          String password = passwordTextField.getText();
-           FileWriter customerNames = new FileWriter("customers.txt");
-           customerNames.write("username" + username );
-           customerNames.write ("password" + password); 
+        String username = usernameTextField.getText();
+        String password = passwordTextField.getText();
+        
+        
+     //will be working hopefully when we can read from a file
+//        if(!(username.equals("") || password.equals(""))){
+//            if(username = a username in the JSON list){
+//                App.setRoot("home");
+//            }else{
+//                messageLabel.setText("ERROR! Customer not in system");
+//        }else{
+//            messageLabel.setText("Must enter fields to login");
+//        }
+//        }
+            
 
-        }
-        //makes it go to the next screen 
-        App.setRoot("secondary");
     }
-
     //method thats called when the register button is hit. we could put a simple form here that also adds them to the file
     @FXML
     private void switchToRegister() throws IOException {
-
+        App.setRoot("register");
         String username = usernameTextField.getText();
-        System.out.print("this is the username they have entered" + username);
-        //get the username and apssword value and do something with it
-        //look at the controller of the other project, see if it makes sense 
-        App.setRoot("secondary");
+
     }
 
     //end of code 
